d588c19195a46783c7dcf2014157106800750541
GaePark <qkrwnsgus01@gmail.com>
2025-05-23 09:23:14 +0900

    00

--- src/test/java/com/sist/web/DevopsApplicationTests.java
+++ src/test/java/com/sist/web/DevopsApplicationTests.java
@@ -1,6 +1,6 @@
 package com.sist.web;
-
-import org.junit.jupiter.api.Test;
+/*
+import org.junit.jupiter.api.Test; 
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -11,3 +11,4 @@ class DevopsApplicationTests {
 	}
 
 }
+*/
